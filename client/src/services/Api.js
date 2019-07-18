febae52f41ef657089f9088a2b0d484fefe742c2
Demaria Woods <dpwoods@uh.edu>
2019-07-18 16:00:45 -0500

    changed base_url

--- client/src/services/Api.js
+++ client/src/services/Api.js
@@ -1,7 +1,8 @@
 
 import axios from 'axios';
 const AxiosInstance = axios.create({
-    baseURL: 'http://localhost:3000/',
+    // baseURL: 'http://localhost:3000/',
+    baseURL: 'https://online-shopping-vue-express.herokuapp.com'
     headers: {
         //'XSRF-TOKEN': `${store.state.csrfToken}`
     }
