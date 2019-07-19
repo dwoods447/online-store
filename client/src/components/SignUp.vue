5c780c68873282229c9c75c2bc419137b7510e89
Demaria Woods <dpwoods@uh.edu>
2019-07-18 20:12:04 -0500

    added vee-validation custom errors messgages

--- client/src/components/SignUp.vue
+++ client/src/components/SignUp.vue
@@ -5,38 +5,48 @@
                     <b-field label="First Name">
                         <b-input v-model="formdata.first_name" v-validate="{required: true}" name="firstname"></b-input>
                     </b-field>
-                    <span>{{ errors.first('firstname') }}</span>
-                      <b-field label="Last Name">
+                    <span class="error">{{ errors.first('firstname') }}</span>
+                    <br/>
+                    <b-field label="Last Name">
                         <b-input v-model="formdata.last_name" v-validate="{required: true}" name="lastname"></b-input>
                     </b-field>
-                    <span>{{ errors.first('lastname') }}</span>
+                    <span class="error">{{ errors.first('lastname') }}</span>
+                    <br/>
                     <b-field label="Phone">
                         <b-input v-model="formdata.phone" v-validate="{required: true}" name="phone"></b-input>
                     </b-field>
-                    <span>{{ errors.first('phone') }}</span>
+                    <span class="error">{{ errors.first('phone') }}</span>
+                    <br/>
                      <b-field label="Address">
                         <b-input v-model="formdata.address" v-validate="{required: true}" name="address"></b-input>
                     </b-field>
-                    <span>{{ errors.first('address') }}</span>
-                      <b-field label="City">
+                    <span class="error">{{ errors.first('address') }}</span>
+                    <br/>
+                    <b-field label="City">
                         <b-input v-model="formdata.city" v-validate="{required: true}" name="city"></b-input>
                     </b-field>
-                    <span>{{ errors.first('city') }}</span>
+                    <span class="error">{{ errors.first('city') }}</span>
+                    <br/>
                     <b-field label="Country">
                         <b-input v-model="formdata.country" v-validate="{required: true}" name="country"></b-input>
                     </b-field>
-                    <span>{{ errors.first('country') }}</span>
+                    <span class="error">{{ errors.first('country') }}</span>
+                    <br/>
                     <b-field label="Email">
                         <b-input v-model="formdata.email" v-validate="{required: true}" name="email"></b-input> 
                     </b-field>
-                    <span>{{ errors.first('email') }}</span>
+                    <span class="error">{{ errors.first('email') }}</span>
+                    <br/>
                     <b-field label="Password">
                         <b-input v-model="formdata.password" v-validate="{required: true}" name="password" type="password"></b-input>
                     </b-field>
+                    <br/>
+                     <span class="error">{{ errors.first('password') }}</span>
                      <b-field label="Confirm Password">
                         <b-input v-model="formdata.confirm_password" v-validate="{required: true, confirmed:formdata.password}" name="password_confirmation" type="password"></b-input>
                     </b-field>
-                    <span>{{ errors.first('password') }}</span>
+                    <span class="error">{{ errors.first('password_confirmation') }}</span>
+                    <br/>
                     <span v-if="message">{{ message}}</span>
                     <router-link :to="{name: 'login'}" style="text-decoration:none;" v-if="message"><b-button>Login</b-button></router-link>
                     <b-button type="is-primary" @click="signup" v-if="!message">Sign Up</b-button>
@@ -47,12 +57,44 @@
 <script>
 import SignUpService from '../services/SignUpService'
 import AuthenticationService from '../services/AuthenticationService'
+import { Validator } from 'vee-validate';
+const dict = {
+    custom: {
+        firstname: {
+            required: 'Please enter your First Name' 
+        },
+        lastname: {
+            required: 'Please enter your Last Name' 
+        },
+        phone:{
+            required: 'Please enter you Phone Number'
+        },
+        address:{
+            required: 'Please enter you Address'
+        },
+        city: {
+            required: 'Please enter your City'
+        },
+        country:{
+            required: 'Please enter your Country'
+        },
+        email: {
+            required: 'Please enter your email'
+        },
+        password: {
+            required: 'Please enter your password'
+        },
+        password_confirmation: {
+            required: 'Please re-enter your password',
+            confirmed: 'Passwords do not match'
+        }
+    }
+};
+Validator.localize('en', dict);
 export default {
     created(){
-         this.getCSRFToken();
     },
     mounted() {
-
     },
     data(){
         return {
@@ -81,15 +123,6 @@ export default {
              });
            
         },
-
-        async getCSRFToken(context){
-            const token = (await AuthenticationService.csrfToken()).data.csrfToken;
-            if(token){  
-                this.$store.dispatch('setStoreCSRFtoken', token);
-                this.formdata._csrf = token;
-            }
-        },
-
         async submitInfo(){
             try {
               const registration = (await SignUpService.signUp(this.formdata)).data;
@@ -110,3 +143,9 @@ export default {
     }
 }
 </script>
+<style scoped>
+  .error{
+      color: red;
+      font-weight: bold;
+  }  
+</style>
--- server/controllers/OrdersController.js
+++ server/controllers/OrdersController.js
@@ -3,10 +3,8 @@ const { Order, OrderProduct, Product }  = require('../models');
 module.exports = {
     async orderProducts(req, res){
         try {
-            
             // Enter data in Orders table
             let todayDate = new Date().toISOString().slice(0,10);
-            // console.log(`Today's Date ${todayDate}`);
             const order  = await Order.create({
                 order_date: todayDate,
                 shipping_address: req.body.shipping.address,
@@ -14,14 +12,12 @@ module.exports = {
                 shipping_country : req.body.shipping.country,
                 CustomerId: req.body.customer.id 
             })
-        //     console.log(`Order: ${JSON.stringify(order)}`);
-        //     console.log(`Shipping Info: ${JSON.stringify(req.body.shipping, null, 2)}`);
-        //    console.log('\r\n\r\n');
-        //    console.log(`Customer Info: ${JSON.stringify(req.body.customer,null, 2)}`);
+            // If the order was saved loop through req body and get products key
           if(order){
             for(let property in req.body){
                 if(req.body.hasOwnProperty(property) && property === 'products'){
                     req.body.products.forEach((product)=> {
+                        // Loop through each product in the array and
                          // Enter data in OrderProducts table 
                         let orderProduct = OrderProduct.create({
                             qty: product.quantity,
@@ -29,17 +25,17 @@ module.exports = {
                             OrderId: order.id,
                             ProductId: product.id
                         })
+                        // If product saved in Order Product table
                         if(orderProduct){
-                            // Decrease product qty's in Products table
+                            // Decrease product's total qty in Products table
                             Product.findOne({
                                 attributes:['qty'], where: {id: product.id}
                             }).then(data => {
-                                console.log(`Data: ${JSON.stringify(data)}`);
                                 if(data){
-                                    console.log(`Total Qty: ${data.qty}`);
-                                    console.log(`Qty Bought: ${product.quantity}`);
-                                    let newQty = parseInt(data.qty) - parseInt(product.quantity)
-                                    console.log(`New Qty: ${newQty}}`);
+                                    // If product was found update its qty attribute
+                                    let totalQty = parseInt(data.qty);
+                                    let qtyOrdered = parseInt(product.quantity);
+                                    let newQty = totalQty - qtyOrdered; 
                                     Product.update({qty: newQty} ,{where:{id: product.id}});
                                  }
                              });
@@ -118,7 +114,7 @@ module.exports = {
                 data: []
             })     
         }
-    }catch(error){
+    } catch(error){
             res.send({
                 error: error
             })
