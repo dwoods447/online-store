9bc86a3d40f858f8578e938f6e24b0a179b7e9d7
Demaria Woods <dpwoods@uh.edu>
2019-07-25 23:40:12 -0500

    generated new products

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -59,6 +59,7 @@ export default {
     methods: {
        async getProducts(){
             const products = (await ProductService.index()).data.data;
+             console.log(`Products ${JSON.stringify(products, null, 2)}`)
             if(products){
                 this.products = [];
                 this.products = products;
--- client/src/components/Product.vue
+++ client/src/components/Product.vue
@@ -18,7 +18,7 @@
 export default {
     props: ['product'],
     created(){
-
+       
     },
     data(){
         return {
