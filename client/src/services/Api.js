454d6d418e74fc41537b7dd7ca7ca9556cd8c145
Demaria Woods <dpwoods@uh.edu>
2019-08-20 15:28:36 -0500

    Added api url to Api.js

--- client/src/services/Api.js
+++ client/src/services/Api.js
@@ -3,8 +3,8 @@ console.log(`Header Token: ${store.getters.getToken}`);
 console.log(`Header Token: ${store.state.jwtToken}`);
 import axios from 'axios';
 const AxiosInstance = axios.create({
-     baseURL: 'http://localhost:3000/',
-     //baseURL: 'https://online-shopping-vue-express.herokuapp.com',
+     //baseURL: 'http://localhost:3000/',
+     baseURL: 'https://online-shopping-vue-express.herokuapp.com',
      headers: {
           Authorization: `Bearer ${store.getters.getToken}`
      }
